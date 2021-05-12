e969a1aa8a6ffd6ffbf341946240ecfcad5210e9
degiorg <gio86rgio@gmail.com>
2021-05-12 02:43:02 +0200

    STEP3

--- src/test/scala/step1/CheckoutTest.scala
+++ src/test/scala/step1/CheckoutTest.scala
@@ -11,7 +11,7 @@ class CheckoutTest extends AnyFlatSpec  with TypeCheckedTripleEquals {
   Checkout(List("")) shouldEqual 0
 }
 """Checkout with an Apple""" should """be 60p""" in {
-  Checkout(List("Apple")) shouldEqual 0.00
+  Checkout(List("Apple")) shouldEqual 0.60
 }
 """Checkout with an Orange""" should """be 25p""" in {
     Checkout(List("Orange")) shouldEqual 0.25
@@ -22,13 +22,27 @@ class CheckoutTest extends AnyFlatSpec  with TypeCheckedTripleEquals {
   """Checkout with 2 Apple and 1 Orange""" should """be 0.85p""" in {
     Checkout(List("Apple", "Apple", "Orange")) shouldEqual 0.85
   }
-  """Checkout with Orange Apple Orange""" should """be 0.50p""" in {
-    Checkout(List("Orange", "Apple", "Orange")) shouldEqual 0.50
+  """Checkout with Orange Apple Orange""" should """be 1.10p""" in {
+    Checkout(List("Orange", "Apple", "Orange")) shouldEqual 1.10
   }
   """Checkout with 3 Orange """ should """be 0.5p""" in {
     Checkout(List("Orange", "Orange", "Orange")) shouldEqual 0.50
   }
   """Checkout with 6 Orange and 1 Apple """ should """be 1""" in {
-    Checkout(List("Orange", "Orange", "Orange", "Apple", "Orange", "Orange", "Orange")) shouldEqual 1.00
+    Checkout(List("Orange", "Orange", "Orange", "Apple", "Orange", "Orange", "Orange")) shouldEqual 1.60
   }
+  """Checkout with 2 Apple and 1 Banana""" should """be 0.80p""" in {
+    Checkout(List("Apple", "Apple", "Banana")) shouldEqual 0.80
+  }
+  """Checkout with 2 Banana and 1 Apple""" should """be 0.80p""" in {
+    Checkout(List("Banana", "Apple", "Banana")) shouldEqual 0.80
+  }
+
+  """Checkout with 2 Banana and 2 Apple""" should """be 0.80p""" in {
+    Checkout(List("Banana", "Apple", "Banana", "Apple")) shouldEqual 0.80
+  }
+  """Checkout with 7 Banana and 2 Apple""" should """be 1.40p""" in {
+    Checkout(List("Banana", "Banana", "Banana", "Banana", "Banana", "Banana", "Apple", "Banana", "Apple")) shouldEqual 1.40
+  }
+
 }
